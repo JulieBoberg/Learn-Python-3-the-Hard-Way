25f5aaa5c82b0aaa3439fc85d01765a18a4361ae
Julie Boberg <43385336+JulieBoberg@users.noreply.github.com>
2019-08-30 15:23:34 -0700

    Project 3
    Added completed project 3 from Learning Python 3 the Hard Way

---
+++ ex3.py
@@ -0,0 +1,60 @@
+# prints "I will now count my chickens"
+print("I will now count my chickens:")
+
+# prints Hens and the number of hens
+print("Hens", 25 + 30 / 6)
+# prints Roosters and the number of Roosters
+print("Roosters", 100 - 25 * 3 % 4)
+# prints text
+print("Now I will count the eggs:")
+# calculates the number of eggs and prints it
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints the sentence including numbers without printing a solution
+print("Is it true that 3 + 2 < 5 - 7?")
+# prints a Boolean value
+print(3 + 2 < 5-1)
+# prints the sentence and after quotes it prints the calculation
+print("what is 3 + 2?", 3 + 2)
+# same as above.
+print("What is 5 - 7?", 5-7)
+#prints the sentence in parenthesis
+print("Oh, that's why it's False.")
+# prints the sentence in parathesis
+print("How about some more.")
+# prints sentence and after the double quotes it performs the comparison and prints a Boolean value
+print("Is it great?", 5 > -2)
+# same as above
+print("Is it greater or equal?", 5 >= -2)
+# same as above
+print("Is it less or equal?", 5 <= -2)
+
+
+# Rewrite with floating point numbers
+
+print("I will now count my chickens:")
+
+print("Hens", 25.0 + 30.0 / 6.0)
+
+print("Roosters", 100.0 - 25.0 * 3.0  % 4.0)
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0)
+
+print("Is it true that 3.0 + 2.0 < 5.0 - 7.0?")
+
+print(3.0 + 2.0 < 5.0 - 1.0)
+
+print("what is 3.0 + 2.0?", 3.0 + 2.0)
+
+print("What is 5.0 - 7.0?", 5.0 - 7.0)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+print("Is it great?", 5.0 > -2.0)
+
+print("Is it greater or equal?", 5.0 >= -2.0)
+
+print("Is it less or equal?", 5.0 <= -2.0)
